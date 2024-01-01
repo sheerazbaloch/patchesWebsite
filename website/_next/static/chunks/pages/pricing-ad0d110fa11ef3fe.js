434c25d0224bc4704cedb9aaa42fd8f25bf825b8
SheerazBaloch <sheeraz.baloch@conurets.com>
2024-01-01 12:08:50 +0500

    work done

---
+++ website/_next/static/chunks/pages/pricing-ad0d110fa11ef3fe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9939],{99425:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/pricing",function(){return u(14703)}])}},function(n){n.O(0,[536,244,5675,785,8410,9774,2888,179],function(){return n(n.s=99425)}),_N_E=n.O()}]);
